7ef6e7c0d720da871c4401cc202ae9604b97578e
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2018-01-08 10:54:09 -0600

    parallel posix

--- Case2/parallel_posix.c
+++ Case2/parallel_posix.c
@@ -11,6 +11,11 @@ int main() {
   FILE *fd;
   int rc;
   MPI_File fdp;
+  MPI_Offset offset, sb_sz;
+  int proc_cnt, k;
+  int sz_superblock = 2048;
+  int superblock[2048];
+  MPI_Status *wstatus;
 
   // Initialize the MPI environment
   MPI_Init(NULL, NULL);
@@ -24,6 +29,21 @@ int main() {
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
   rc = MPI_File_open( MPI_COMM_WORLD, "datafile", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fdp);
+
+  sb_sz = 0;
+  proc_cnt = 0;
+  offset = 0;
+
+  for(k = 0; k < sz_superblock; ++k ) {
+    superblock[k] = k;
+  }
+
+  if(myid == proc_cnt){
+    MPI_File_write_all(fdp, superblock, sz_superblock, MPI_INTEGER, wstatus);
+  } else {
+    MPI_File_write_all(fdp, superblock, 0, MPI_INTEGER, wstatus);
+  }
+
   MPI_File_close( &fdp );
 
   MPI_Barrier(MPI_COMM_WORLD);
