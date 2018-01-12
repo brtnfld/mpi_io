2c77da98d05bf671ef54a4dc4484658c5f364d3f
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2018-01-12 15:40:19 -0600

    changed to mimic pagebuffer writes

--- Case2/case2.F90
+++ Case2/case2.F90
@@ -20,6 +20,8 @@ PROGRAM case2
      END FUNCTION ctrunc
   END INTERFACE
 
+  INTEGER, PARAMETER :: ndsets = 5
+
   INTEGER(KIND=int64), PARAMETER :: MegaB = 134217728_int64 !2097152_int64
 
 ! 2**20
@@ -49,11 +51,12 @@ PROGRAM case2
   
   INTEGER :: filetype, contig
   INTEGER (KIND=MPI_ADDRESS_KIND) :: extent
-  INTEGER(KIND=MPI_OFFSET_KIND) :: disp, offset, expand_fs,sb_sz
-  INTEGER, ALLOCATABLE, DIMENSION(:) :: buf
+  INTEGER(KIND=MPI_OFFSET_KIND) :: disp, offset, expand_fs, sb_sz
+  INTEGER :: n_pgbuf
+  INTEGER, ALLOCATABLE, DIMENSION(:) :: pgbuf
   DOUBLE PRECISION, DIMENSION(1:3) :: t
   DOUBLE PRECISION :: t1, t2, t3
-  INTEGER :: bufsize
+  INTEGER :: n_rawbuf
   CHARACTER(len=1) :: argv
   LOGICAL :: exist
   CHARACTER(len=128) :: arg
@@ -63,7 +66,8 @@ PROGRAM case2
 
   INTEGER, PARAMETER :: sz_superblock = 2048 ! 8,192 Bytes
   INTEGER*4, DIMENSION(1:sz_superblock) :: superblock
-  INTEGER, DIMENSION(:), ALLOCATABLE :: mb_buf
+  INTEGER(KIND=int64), DIMENSION(1:2) :: mb_offsets
+  INTEGER(KIND=int64) :: mb_size
 
   CALL MPI_Init(ierr)
   CALL MPI_Comm_size(MPI_COMM_WORLD, nprocs, ierr)
@@ -72,6 +76,9 @@ PROGRAM case2
   IF(rank.EQ.0)  CALL EXECUTE_COMMAND_LINE("rm -f datafile.mpio")
   CALL MPI_Barrier(MPI_COMM_WORLD, ierr)
 
+  ! GET COMMAND LINE OPITIONS
+  ! 1 -- use MPI IO to truncate the file
+  ! 2 -- use POSIX to truncate the file
   k = 0
   argv=""
   DO
@@ -81,12 +88,24 @@ PROGRAM case2
      k = k + 1
   END DO
 
+  ! page buffer size
+  n_pgbuf = 4*MegaB/sizeof(k)/512
+  ALLOCATE(pgbuf(1:n_pgbuf))
+  pgbuf(:) = rank
+
+  sb_sz = 0
+  offset = 0
+
+  mb_offsets(1) = offset
+
+
+  mb_size = sizeof(pgbuf)
+  offset = offset + mb_size
+
   CALL MPI_File_open(MPI_COMM_WORLD, "datafile.mpio",     &
        IOR(MPI_MODE_CREATE,MPI_MODE_WRONLY), &
        MPI_INFO_NULL, fh, ierr)
 
-  sb_sz = 0
-  offset = 0
 
 ! WRITE OUR PSEUDO SUPERBLOCK COLLECTIVELY,
 ! ONLY PROC 0 WRITES SOMETHING.
@@ -107,40 +126,45 @@ PROGRAM case2
      IF(rank.EQ.0) PRINT*, "ERROR: SIZE OF DATASET NOT DIVISABLE BY NUMBER OF PROCESSES"
      CALL MPI_ABORT(MPI_COMM_WORLD, -1, ierr)
   ELSE
-     bufsize = N/nprocs
+     n_rawbuf = N/nprocs
   ENDIF
 
-! A) RAW DATA WRITES, 5 SETS
+! A) RAW DATA WRITES, ndsets  SETS
 
-  DO k = 1, 5
+  DO k = 1, ndsets
+
+     IF(k.EQ.ndsets)THEN
+        ! where to write the final page buffer, will be writen by proc 1
+        mb_offsets(2) = offset
+
+        offset = offset + mb_size
+
+     ENDIF
 
      ! A.1) WRITE RAW DATA COLLECTIVELY; ALL PROCESSES CONTRIBUTE TO WRITING A SECTION
      !      OF THE DATA SET.
 
-     bufsize = N/nprocs
+     n_rawbuf = N/nprocs
 
-     CALL raw(fh, rank, nprocs, bufsize, offset)
+     CALL raw(fh, rank, nprocs, n_rawbuf, offset)
      
   ENDDO
 
-! B) WRITE THE RAW DATA, COMBINE THE FOUR METADATA WRITES INTO ONE. WRITEN BY PROCESS ZERO
-!    EACH PROCESS WRITE 1MB     
-  bufsize = 4*MegaB/sizeof(k)/512
-        
-  ALLOCATE(buf(1:bufsize))
-        
-  buf(:) = rank
+! B) WRITE THE PAGEBUFFER
+!     * process 0 writes 1MB at offset 0 in the file
+!     * process 1 writes 1MB in the file after the N-1 dataset raw data
+   
+
+!  IF(rank.LE.1)THEN
+!     offset = offset + rank*sizeof(buf)
+!  ENDIF
 
   IF(rank.LE.1)THEN
-     offset = offset + rank*sizeof(buf)
-  ENDIF
-
-  CALL MPI_File_set_view(fh, offset, MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
-
-  IF(rank.LE.1)THEN
-     CALL MPI_File_write_all(fh, buf, bufsize, MPI_INTEGER, wstatus, ierr)
+     CALL MPI_File_set_view(fh, mb_offsets(rank+1), MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
+     CALL MPI_File_write_all(fh, pgbuf, n_pgbuf, MPI_INTEGER, wstatus, ierr)
   ELSE
-     CALL MPI_File_write_all(fh, buf, 0, MPI_INTEGER, wstatus, ierr)
+     CALL MPI_File_set_view(fh, 0, MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
+     CALL MPI_File_write_all(fh, pgbuf, 0, MPI_INTEGER, wstatus, ierr)
   ENDIF
 
 ! EXPAND THE FILE
