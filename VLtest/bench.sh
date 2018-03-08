3aede59801a06669703bd6867847ca88283d5039
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2018-03-08 15:09:36 -0600

    script to run benchmark

---
+++ VLtest/bench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+HOST=$(uname -n)
+RUN=''
+if [ "$HOST" == 'jelly' ];then
+    RUN="$HOME/packages/nocache/nocache"
+fi
+
+nt=10
+##for j in 4096 8192 16384 32768 65536 131072 262144
+for j in 4096 8192 16384 32768 65536 131072 262144
+do
+ rm -f VL_timing.txt
+ for i in `seq 1 $nt`
+  do
+    $RUN a.out $j
+  done
+  perl -lane 'for $c (0..$#F){$t[$c] += $F[$c]}; END{for $c (0..$#t){print $t[$c]/$.}}' VL_timing.txt >> $j
+done
