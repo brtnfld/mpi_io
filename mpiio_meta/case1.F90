f32d928d1db158d74d9ed4ddea7a543cd21891e0
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2018-09-07 09:42:17 -0500

    added options for bcasts

--- mpiio_meta/case1.F90
+++ mpiio_meta/case1.F90
@@ -1,11 +1,7 @@
-PROGRAM case1
+#define DEBUG 0
 
-! Large chunks of data are interspersed with randomly scattered 
-! small clusters of small (i.e. < 5 KiB) pieces of data totaling 1-2 MiB. 
-! Large data is written in several collective writes.  
-! Small data is also written collectively just before file close, with at 
-! most one piece of small data per process.  File is truncated 
-! to its current size at close via MPI_File_set_size()
+
+PROGRAM case1
 
   USE mpi
   USE iso_c_binding
@@ -21,7 +17,9 @@ PROGRAM case1
   
   INTEGER(KIND=MPI_OFFSET_KIND) :: offset
   INTEGER, PARAMETER :: bufsize = 8
+  INTEGER :: fullbufsize
   INTEGER, DIMENSION(1:bufsize) :: buf
+  INTEGER, ALLOCATABLE, DIMENSION(:) :: fullbuf
   DOUBLE PRECISION, DIMENSION(1:1) :: t
   DOUBLE PRECISION :: t1, t2, t3
   CHARACTER(len=1) :: argv
@@ -32,19 +30,19 @@ PROGRAM case1
 
   INTEGER*4, DIMENSION(1:N) :: ndata
   INTEGER:: proc_cnt
-  INTEGER :: incr
+  INTEGER :: incr, icnt, narg
 
   CALL MPI_Init(ierr)
   CALL MPI_Comm_size(MPI_COMM_WORLD, nprocs, ierr)
   CALL MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
 
-  k = 0
+  narg = 0
   argv=""
   DO
-     CALL get_command_argument(k, arg)
+     CALL get_command_argument(narg, arg)
      IF (LEN_TRIM(arg) == 0) EXIT
      argv(1:1) = arg(1:1)
-     k = k + 1
+     narg = narg + 1
   END DO
 
   IF(rank.EQ.0)THEN
@@ -64,31 +62,108 @@ PROGRAM case1
 
   CALL MPI_Barrier(MPI_COMM_WORLD, ierr)
 
-  CALL MPI_File_open(MPI_COMM_WORLD, "datafile.mpio",     &
-       MPI_MODE_RDWR, &
-       MPI_INFO_NULL, fh, ierr)
-
   incr = 64
-  t(1) = 0.
   offset = 0
-  DO k = 1, N, incr
-    CALL MPI_File_set_view(fh, offset, MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
-    t1 = MPI_Wtime()
-    CALL MPI_File_read_all(fh, buf, bufsize, MPI_INT, wstatus, ierr );
-    t(1) = t(1) + (MPI_Wtime() - t1);
-    offset = offset + incr*sizeof(k)
+  t(1) = 0.
 
-#if 0
-    IF(rank.EQ.1)THEN
-       DO i = 1, bufsize
-          PRINT*,buf(i)
-       ENDDO
-    ENDIF
+  IF( narg .EQ. 1)THEN
+     CALL MPI_File_open(MPI_COMM_WORLD, "datafile.mpio",     &
+          MPI_MODE_RDWR, &
+          MPI_INFO_NULL, fh, ierr)
+     
+     DO k = 1, N, incr
+        CALL MPI_File_set_view(fh, offset, MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
+        t1 = MPI_Wtime()
+        CALL MPI_File_read_all(fh, buf, bufsize, MPI_INTEGER, wstatus, ierr );
+        t(1) = t(1) + (MPI_Wtime() - t1);
+        offset = offset + incr*sizeof(k)
+
+#if DEBUG
+        IF(rank.EQ.1)THEN
+           DO i = 1, bufsize
+              PRINT*,buf(i)
+           ENDDO
+        ENDIF
 #endif
-  ENDDO
+     ENDDO
 
-  CALL MPI_File_close(fh, ierr)
-  
+     CALL MPI_File_close(fh, ierr)
+  ENDIF
+
+! Read on proc 0, and broadcast
+
+
+! OPTION 1
+
+  IF( argv .EQ. '1')THEN
+
+     fullbufsize = N/incr*bufsize
+     ALLOCATE(fullbuf(1:fullbufsize))
+
+     IF(rank.EQ.0)THEN
+
+        CALL MPI_File_open(MPI_COMM_SELF, "datafile.mpio",     &
+             MPI_MODE_RDWR, &
+             MPI_INFO_NULL, fh, ierr)
+
+        icnt = 1
+        DO k = 1, N, incr
+           CALL MPI_File_set_view(fh, offset, MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
+           t1 = MPI_Wtime()
+           CALL MPI_File_read_all(fh, fullbuf(icnt), bufsize, MPI_INTEGER, wstatus, ierr );
+           t(1) = t(1) + (MPI_Wtime() - t1);
+           offset = offset + incr*sizeof(k)
+           icnt = icnt + bufsize
+        ENDDO
+        CALL MPI_File_close(fh, ierr)        
+     ENDIF
+     
+     t1 = MPI_Wtime()
+     CALL MPI_BCAST(fullbuf, fullbufsize, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+     t(1) = t(1) + (MPI_Wtime() - t1);
+#if DEBUG    
+     IF(rank.EQ.1)THEN
+        DO i = 1, fullbufsize
+           PRINT*,fullbuf(i)
+        ENDDO
+     ENDIF
+#endif     
+     DEALLOCATE( fullbuf )
+  ENDIF
+
+! OPTION 2
+
+  IF( argv .EQ. '2')THEN
+
+     IF(rank.EQ.0)THEN
+        CALL MPI_File_open(MPI_COMM_SELF, "datafile.mpio",     &
+             MPI_MODE_RDWR, &
+             MPI_INFO_NULL, fh, ierr)
+     ENDIF
+
+     DO k = 1, N, incr
+        t1 = MPI_Wtime()
+        IF(rank.EQ.0)THEN
+           CALL MPI_File_set_view(fh, offset, MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
+           CALL MPI_File_read_all(fh, buf, bufsize, MPI_INTEGER, wstatus, ierr );
+        ENDIF
+        CALL MPI_BCAST(buf, bufsize, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+        t(1) = t(1) + (MPI_Wtime() - t1);
+        offset = offset + incr*sizeof(k)
+#if DEBUG
+        IF(rank.EQ.1)THEN
+           DO i = 1, bufsize
+              PRINT*,buf(i)
+           ENDDO
+        ENDIF
+#endif
+     ENDDO
+     
+     IF(rank.EQ.0)THEN
+        CALL MPI_File_close(fh, ierr)
+     ENDIF
+  ENDIF
+
   CALL MPI_reduce(t, t1, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr);
   CALL MPI_reduce(t, t2, 1, MPI_DOUBLE_PRECISION, MPI_MIN, 0, MPI_COMM_WORLD, ierr);
   CALL MPI_reduce(t, t3, 1, MPI_DOUBLE_PRECISION, MPI_MAX, 0, MPI_COMM_WORLD, ierr);
