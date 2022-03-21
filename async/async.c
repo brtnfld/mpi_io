6dd4fea0e787f3beaf7d4337fe0a33265d221e78
Scot Breitenfeld <brtnfld@hdfgroup.org>
2022-03-21 16:21:42 -0500

    misc. daos

--- async/async.c
+++ async/async.c
@@ -58,24 +58,25 @@ int main(int ac, char **av)
 {
     int  mpi_size, mpi_rank;
     MPI_File fh;
-    char *filename = "./mpitest.data";
+    //   char *filename = "daos:mpitest.data";
+    char *filename = "mpitest.data";
     char mpi_err_str[MPI_MAX_ERROR_STRING];
     int  mpi_err_strlen;
     int  mpi_err;
     char expect_val;
     int  i=0 ; 
-    int  nerrors = 0;		/* number of errors */
+    int  nerrors = 0;           /* number of errors */
     /* buffer size is the total size for one variable. */
     // This will make the variable size 5GB(5*1024*1024*1024). 
     // Uncomment the following line
-    //int64_t  buf_size = 5368709120LL;
+    //MSB int64_t  buf_size = 5368709120LL;
     //Uncomment the following line makes the variable size 50GB.
     //int64_t  buf_size = 53687091200LL;
     
     //int64_t  buf_size = 536870912LL;
     int64_t  buf_size; // = 10240LL;
     /* Number of variables, currently is 9 like Generic IO. */
-    int num_vars  = 2;
+    int num_vars  = 2; //10;
     MPI_Offset  mpi_off = 0;
     MPI_Status  mpi_stat;
     char* writedata = NULL;
@@ -97,25 +98,25 @@ int main(int ac, char **av)
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-    buf_size = 1*1024*1024*1024;
+//    buf_size = 5*1024*1024*1024;
 
     /* Buffer size per process per variable, The smallest unit for one process to write */
     buf_size_per_proc = buf_size/mpi_size;
 
     if (mpi_rank==0){
-	printf("Testing simple C MPI collective IO program with %d processes accessing file %s\n",
-	    mpi_size, filename);
+        printf("Testing simple C MPI collective IO program with %d processes accessing file %s\n",
+            mpi_size, filename);
         printf("This tests the MPIO different patterns for %d variables with MPIO write.\n", num_vars);
     }
 
     if ((mpi_err = MPI_File_open(MPI_COMM_WORLD, filename,
-	    MPI_MODE_RDWR | MPI_MODE_CREATE ,
-	    info, &fh))
-	    != MPI_SUCCESS){
-	    MPI_Error_string(mpi_err, mpi_err_str, &mpi_err_strlen);
-	    PRINTID;
-	    printf("MPI_File_open failed (%s)\n", mpi_err_str);
-	    MPI_Abort(MPI_COMM_WORLD, 1);
+            MPI_MODE_RDWR | MPI_MODE_CREATE ,
+            info, &fh))
+            != MPI_SUCCESS){
+            MPI_Error_string(mpi_err, mpi_err_str, &mpi_err_strlen);
+            PRINTID;
+            printf("MPI_File_open failed (%s)\n", mpi_err_str);
+            MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     // Allocate total amount of data to the buffer
@@ -130,8 +131,11 @@ int main(int ac, char **av)
     mpi_off = buf_size_per_proc*mpi_rank;
 
     for (i=0; i < num_vars; i++) {
-      if ((mpi_err = MPI_File_iwrite_at_all(fh, mpi_off, writedata, buf_size_per_proc, MPI_BYTE,&request[i]))
+     // if ((mpi_err = MPI_File_iwrite_at_all(fh, mpi_off, writedata, buf_size_per_proc, MPI_BYTE,&request[i]))
+     //     != MPI_SUCCESS){
+     if ((mpi_err = MPI_File_iwrite_at(fh, mpi_off, writedata, buf_size_per_proc, MPI_BYTE,&request[i]))
           != MPI_SUCCESS){
+
         MPI_Error_string(mpi_err, mpi_err_str, &mpi_err_strlen);
         PRINTID;
         printf("MPI_File_write_at_all offset(%ld), bytes (%d), failed (%s)\n",
@@ -140,15 +144,17 @@ int main(int ac, char **av)
       }
       mpi_off+=buf_size;     
     }
-
+#if 1 
     mpiio_stime = MPI_Wtime();
-    PImpi(mpi_rank, mpi_size, 1E8);
+    for (i=0; i < 2; i++) {
+       PImpi(mpi_rank, mpi_size, 1E9);
+    } 
     mpiio_etime = MPI_Wtime();
     total_time = mpiio_etime - mpiio_stime;
     if(mpi_rank == 0) {
       printf("Compute time %f seconds. \n", total_time);
     }
-
+#endif
     mpiio_stime = MPI_Wtime();
     for (i=0; i < num_vars; i++) {
       MPI_Wait( &request[i], &mpi_stat);
@@ -163,12 +169,12 @@ int main(int ac, char **av)
     if(mpi_rank == 0) {
       rate = (double)(buf_size*num_vars)/Max_total_time/(1024.*1024.);
       printf("%d Procs Wrote %d variables in %f seconds. \n",mpi_size,num_vars,Max_total_time);
-      printf(" Bandwidth is %f MB/s.\n",rate);
-      printf("Total IO time for all processes is %f seconds.\n",Sum_total_time);
-      printf("Minimum IO time for all processes is %f seconds.\n",Min_total_time);
-      rate = (double)(buf_size*num_vars)/(Sum_total_time/mpi_size)/(1024.*1024.);
-      printf("Average IO time for all processes is %f seconds.\n",Sum_total_time/mpi_size);
-      printf(" Average Bandwidth is %f MB/s.\n",rate);
+     // printf(" Bandwidth is %f MB/s.\n",rate);
+      //printf("Total IO time for all processes is %f seconds.\n",Sum_total_time);
+      //printf("Minimum IO time for all processes is %f seconds.\n",Min_total_time);
+      //rate = (double)(buf_size*num_vars)/(Sum_total_time/mpi_size)/(1024.*1024.);
+      //printf("Average IO time for all processes is %f seconds.\n",Sum_total_time/mpi_size);
+     // printf(" Average Bandwidth is %f MB/s.\n",rate);
     }
 
     free(writedata);
@@ -179,31 +185,32 @@ int main(int ac, char **av)
 #if 0
     /* each process reads all data and verify. */
     for (irank=0; irank < mpi_size; irank++){
-	mpi_off = irank*DIMSIZE;
-	if ((mpi_err = MPI_File_read_at(fh, mpi_off, readdata, DIMSIZE, MPI_BYTE,
-		&mpi_stat))
-		!= MPI_SUCCESS){
-	    MPI_Error_string(mpi_err, mpi_err_str, &mpi_err_strlen);
-	    PRINTID;
-	    printf("MPI_File_read_at offset(%ld), bytes (%d), failed (%s)\n",
-		    (long) mpi_off, (int) DIMSIZE, mpi_err_str);
-	    MPI_Abort(MPI_COMM_WORLD, 1);
-	};
-	for (i=0; i < DIMSIZE; i++){
-	    expect_val = irank*DIMSIZE + i;
-	    if (readdata[i] != expect_val){
-		PRINTID;
-		printf("read data[%d:%d] got %d, expect %d\n", irank, i,
-			readdata[i], expect_val);
-		nerrors++;
-	    }
-	}
+        mpi_off = irank*DIMSIZE;
+        if ((mpi_err = MPI_File_read_at(fh, mpi_off, readdata, DIMSIZE, MPI_BYTE,
+                &mpi_stat))
+                != MPI_SUCCESS){
+            MPI_Error_string(mpi_err, mpi_err_str, &mpi_err_strlen);
+            PRINTID;
+            printf("MPI_File_read_at offset(%ld), bytes (%d), failed (%s)\n",
+                    (long) mpi_off, (int) DIMSIZE, mpi_err_str);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        };
+        for (i=0; i < DIMSIZE; i++){
+            expect_val = irank*DIMSIZE + i;
+            if (readdata[i] != expect_val){
+                PRINTID;
+                printf("read data[%d:%d] got %d, expect %d\n", irank, i,
+                        readdata[i], expect_val);
+                nerrors++;
+            }
+        }
     }
     if (nerrors)
-	MPI_Abort(MPI_COMM_WORLD, 1);
+        MPI_Abort(MPI_COMM_WORLD, 1);
     MPI_File_close(&fh);
 #endif
 
-
     return 0;
 }
+
+
