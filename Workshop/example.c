d200010ae9f1384326f2abe7f2ee41eb75426b98
Scot Breitenfeld <brtnfld@hdfgroup.org>
2022-08-17 10:41:36 -0500

    fix

--- Workshop/example.c
+++ Workshop/example.c
@@ -88,151 +88,162 @@ main (int argc, char **argv)
      *  \_/ \_/ \_/ \_/ \_/
      *
      */
-    if(mpi_rank == 0) {
-      /*
-       * Set up file access property list with parallel I/O access
-       */
+    
+    if(write ==1) {
+      if(mpi_rank == 0) {
+        /*
+         * Set up file access property list with parallel I/O access
+         */
+        plist_id = H5Pcreate(H5P_FILE_ACCESS);
+        H5Pset_libver_bounds(plist_id, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST);
+        hid_t dcpl_id = H5Pcreate(H5P_DATASET_CREATE);
+        H5Pset_alloc_time(dcpl_id, H5D_ALLOC_TIME_EARLY);
+        H5Pset_fill_time(dcpl_id, H5D_FILL_TIME_NEVER);
+        
+        /*
+         * Create a new file collectively and release property list identifier.
+         */
+        file_id = H5Fcreate(H5FILE_NAME, H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
+        H5Pclose(plist_id);
+        
+        /*
+         * Create the dataspace for the dataset.
+         */
+        filespace = H5Screate_simple(1, dimsf, NULL);
+        
+        for (i=0; i < NDSETS; i++) {
+          snprintf(dset_name, 12, "%s_%06d", DSET_NAME, i);
+          /*
+           * Create the dataset with default properties and close filespace.
+           */
+          dset_id = H5Dcreate(file_id, dset_name, H5T_NATIVE_INT, filespace, H5P_DEFAULT, dcpl_id, H5P_DEFAULT);
+          H5Dclose(dset_id);
+        }
+        H5Sclose(filespace);
+        H5Fclose(file_id);
+        H5Pclose(dcpl_id);
+      }
+      
+      MPI_Barrier(comm);
+      
       plist_id = H5Pcreate(H5P_FILE_ACCESS);
-      H5Pset_libver_bounds(plist_id, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST);
-      hid_t dcpl_id = H5Pcreate(H5P_DATASET_CREATE);
-      H5Pset_alloc_time(dcpl_id, H5D_ALLOC_TIME_EARLY);
-      H5Pset_fill_time(dcpl_id, H5D_FILL_TIME_NEVER);
-
+      H5Pset_fapl_mpio(plist_id, comm, info);
+      
       /*
        * Create a new file collectively and release property list identifier.
        */
-      file_id = H5Fcreate(H5FILE_NAME, H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
+      file_id = H5Fopen(H5FILE_NAME, H5F_ACC_RDWR, plist_id);
       H5Pclose(plist_id);
       
       /*
        * Create the dataspace for the dataset.
        */
       filespace = H5Screate_simple(1, dimsf, NULL);
+
+      /*
+       * Each process defines dataset in memory and writes it to the hyperslab
+       * in the file.
+       */
+      count[0] =  dimsf[0]/mpi_size; 
+      offset[0] = (mpi_rank)*count[0];
+      memspace = H5Screate_simple(1, count, NULL);
+
+      /*
+       * Initialize data buffer
+       */
+      data1 = (int *) malloc(sizeof(int)*count[0]);
+      data2 = (int *) malloc(sizeof(int)*count[0]);
+      data3 = (int *) malloc(sizeof(int)*count[0]);
+      data4 = (int *) malloc(sizeof(int)*count[0]);
+      data5 = (int *) malloc(sizeof(int)*count[0]);
+      data6 = (int *) malloc(sizeof(int)*count[0]);
+      data7 = (int *) malloc(sizeof(int)*count[0]);
+      data8 = (int *) malloc(sizeof(int)*count[0]);
+      data9 = (int *) malloc(sizeof(int)*count[0]);
       
-      for (i=0; i < NDSETS; i++) {
-        snprintf(dset_name, 12, "%s_%06d", DSET_NAME, i);
-        /*
-         * Create the dataset with default properties and close filespace.
-         */
-        dset_id = H5Dcreate(file_id, dset_name, H5T_NATIVE_INT, filespace, H5P_DEFAULT, dcpl_id, H5P_DEFAULT);
-        H5Dclose(dset_id);
+      for (i=0; i < count[0]; i++) {
+        data1[i] = mpi_rank+10;
+        data2[i] = mpi_rank+10;
+        data3[i] = mpi_rank+10;
+        data4[i] = mpi_rank+10;
+        data5[i] = mpi_rank+10;
+        data6[i] = mpi_rank+10;
+        data7[i] = mpi_rank+10;
+        data8[i] = mpi_rank+10;
+        data9[i] = mpi_rank+10;
       }
+
+      //  printf("proc %d: count NX = %ld \n", mpi_rank, count[0]);
+      // printf("proc %d: offset NX = %ld \n", mpi_rank, offset[0]);
+      
+      /*
+       * Select hyperslab in the file.
+       */
+      H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
+      
+      plist_id = H5Pcreate(H5P_DATASET_XFER);
+      if(collective == 1)
+        H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
+      
+      timer_tick(&write_time, comm, 1);
+      
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 0);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data1);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 1);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data2);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 2);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data3);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 3);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data4);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 4);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data5);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 5);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data6);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 6);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data7);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 7);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data8);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 8);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data9);
+      H5Dclose(dset_id);
+
+      timer_tock(&write_time);
+      timer_collectprintstats(write_time, comm, 0, "Time for H5Dwrite to complete");
+
+      /*
+       * Close/release resources.
+       */
+      H5Pclose(plist_id);
+      H5Sclose(memspace);
       H5Sclose(filespace);
       H5Fclose(file_id);
-      H5Pclose(dcpl_id);
+      free(data1);
+      free(data2);
+      free(data3);
+      free(data4);
+      free(data5);
+      free(data6);
+      free(data7);
+      free(data8);
+      free(data9);
     }
-    
-    MPI_Barrier(comm);
-
-    plist_id = H5Pcreate(H5P_FILE_ACCESS);
-    H5Pset_fapl_mpio(plist_id, comm, info);
-
-    /*
-     * Create a new file collectively and release property list identifier.
-     */
-    file_id = H5Fopen(H5FILE_NAME, H5F_ACC_RDWR, plist_id);
-    H5Pclose(plist_id);
-
-    /*
-     * Create the dataspace for the dataset.
-     */
-    filespace = H5Screate_simple(1, dimsf, NULL);
-
-    /*
-     * Each process defines dataset in memory and writes it to the hyperslab
-     * in the file.
-     */
-    count[0] =  dimsf[0]/mpi_size; 
-    offset[0] = (mpi_rank)*count[0];
-    memspace = H5Screate_simple(1, count, NULL);
-
-    /*
-     * Initialize data buffer
-     */
-    data1 = (int *) malloc(sizeof(int)*count[0]);
-    data2 = (int *) malloc(sizeof(int)*count[0]);
-    data3 = (int *) malloc(sizeof(int)*count[0]);
-    data4 = (int *) malloc(sizeof(int)*count[0]);
-    data5 = (int *) malloc(sizeof(int)*count[0]);
-    data6 = (int *) malloc(sizeof(int)*count[0]);
-    data7 = (int *) malloc(sizeof(int)*count[0]);
-    data8 = (int *) malloc(sizeof(int)*count[0]);
-    data9 = (int *) malloc(sizeof(int)*count[0]);
-
-    for (i=0; i < count[0]; i++) {
-      data1[i] = mpi_rank+10;
-      data2[i] = mpi_rank+10;
-      data3[i] = mpi_rank+10;
-      data4[i] = mpi_rank+10;
-      data5[i] = mpi_rank+10;
-      data6[i] = mpi_rank+10;
-      data7[i] = mpi_rank+10;
-      data8[i] = mpi_rank+10;
-      data9[i] = mpi_rank+10;
-    }
-
-    //  printf("proc %d: count NX = %ld \n", mpi_rank, count[0]);
-    // printf("proc %d: offset NX = %ld \n", mpi_rank, offset[0]);
-
-    /*
-     * Select hyperslab in the file.
-     */
-    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
-
-    plist_id = H5Pcreate(H5P_DATASET_XFER);
-    if(collective == 1)
-      H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
-
-    timer_tick(&write_time, comm, 1);
-
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 0);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data1);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 1);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data2);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 2);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data3);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 3);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data4);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 4);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data5);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 5);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data6);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 6);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data7);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 7);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data8);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 8);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data9);
-    H5Dclose(dset_id);
-
-    timer_tock(&write_time);
-    timer_collectprintstats(write_time, comm, 0, "Time for H5Dwrite to complete");
-
-    /*
-     * Close/release resources.
-     */
-    H5Pclose(plist_id);
-    H5Sclose(memspace);
-    H5Sclose(filespace);
-    H5Fclose(file_id);
-
     /*
      *   _   _   _   _
      *  / \ / \ / \ / \
@@ -240,96 +251,108 @@ main (int argc, char **argv)
      *  \_/ \_/ \_/ \_/
      */
 
-    plist_id = H5Pcreate(H5P_FILE_ACCESS);
-    H5Pset_fapl_mpio(plist_id, comm, info);
+    if( read == 1) {
+      plist_id = H5Pcreate(H5P_FILE_ACCESS);
+      H5Pset_fapl_mpio(plist_id, comm, info);
 
-    /*
-     * Create a new file collectively and release property list identifier.
-     */
-    file_id = H5Fopen(H5FILE_NAME, H5F_ACC_RDONLY, plist_id);
-    H5Pclose(plist_id);
+      /*
+       * Create a new file collectively and release property list identifier.
+       */
+      file_id = H5Fopen(H5FILE_NAME, H5F_ACC_RDONLY, plist_id);
+      H5Pclose(plist_id);
+      
+      /*
+       * Create the dataspace for the dataset.
+       */
+      filespace = H5Screate_simple(1, dimsf, NULL);
 
-    /*
-     * Create the dataspace for the dataset.
-     */
-    filespace = H5Screate_simple(1, dimsf, NULL);
+      /*
+       * Each process defines dataset in memory and writes it to the hyperslab
+       * in the file.
+       */
+      count[0] =  dimsf[0]/mpi_size; 
+      offset[0] = (mpi_rank)*count[0];
+      memspace = H5Screate_simple(1, count, NULL);
+      
+      data1 = (int *) malloc(sizeof(int)*count[0]);
+      data2 = (int *) malloc(sizeof(int)*count[0]);
+      data3 = (int *) malloc(sizeof(int)*count[0]);
+      data4 = (int *) malloc(sizeof(int)*count[0]);
+      data5 = (int *) malloc(sizeof(int)*count[0]);
+      data6 = (int *) malloc(sizeof(int)*count[0]);
+      data7 = (int *) malloc(sizeof(int)*count[0]);
+      data8 = (int *) malloc(sizeof(int)*count[0]);
+      data9 = (int *) malloc(sizeof(int)*count[0]);
 
-    /*
-     * Each process defines dataset in memory and writes it to the hyperslab
-     * in the file.
-     */
-    count[0] =  dimsf[0]/mpi_size; 
-    offset[0] = (mpi_rank)*count[0];
-    memspace = H5Screate_simple(1, count, NULL);
+      /*
+       * Select hyperslab in the file.
+       */
+      H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
 
-    /*
-     * Select hyperslab in the file.
-     */
-    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
+      plist_id = H5Pcreate(H5P_DATASET_XFER);
+      if(collective == 1)
+        H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
+      
+      timer_tick(&read_time, comm, 1);
+      
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 0);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data1);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 1);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data2);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 2);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data3);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 3);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data4);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 4);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data5);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 5);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data6);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 6);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data7);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 7);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data8);
+      H5Dclose(dset_id);
+      snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 8);
+      dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+      H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data9);
+      H5Dclose(dset_id);
+      
+      timer_tock(&read_time);
+      timer_collectprintstats(read_time, comm, 0, "Time for H5Dread  to complete");
+      
+      free(data1);
+      free(data2);
+      free(data3);
+      free(data4);
+      free(data5);
+      free(data6);
+      free(data7);
+      free(data8);
+      free(data9);
 
-    plist_id = H5Pcreate(H5P_DATASET_XFER);
-    if(collective == 1)
-      H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
-
-    timer_tick(&read_time, comm, 1);
-
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 0);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data1);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 1);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data2);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 2);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data3);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 3);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data4);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 4);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data5);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 5);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data6);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 6);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data7);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 7);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data8);
-    H5Dclose(dset_id);
-    snprintf(dset_name, 12, "%s_%06d",  DSET_NAME, 8);
-    dset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-    H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data9);
-    H5Dclose(dset_id);
-
-    timer_tock(&read_time);
-    timer_collectprintstats(read_time, comm, 0, "Time for H5Dread  to complete");
-
-    free(data1);
-    free(data2);
-    free(data3);
-    free(data4);
-    free(data5);
-    free(data6);
-    free(data7);
-    free(data8);
-    free(data9);
-
-    /*
-     * Close/release resources.
-     */
-    H5Pclose(plist_id);
-    H5Sclose(memspace);
-    H5Sclose(filespace);
-    H5Fclose(file_id);
+      /*
+       * Close/release resources.
+       */
+      H5Pclose(plist_id);
+      H5Sclose(memspace);
+      H5Sclose(filespace);
+      H5Fclose(file_id);
+    }
 
     MPI_Finalize();
 
